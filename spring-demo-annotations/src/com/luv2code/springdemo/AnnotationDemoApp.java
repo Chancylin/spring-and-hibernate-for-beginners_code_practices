7904eee041a05822b3a904d1d1d51b6c46ece654
Chuangxin Lin <lcxustc@gmail.com>
2022-07-19 22:16:15 -0400

    Section 8: Spring configuration with Java Annotations - Dependency Injection. Constructor Injection.

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
+		
 		// read spring config file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
@@ -13,6 +14,9 @@ public class AnnotationDemoApp {
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		// call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
