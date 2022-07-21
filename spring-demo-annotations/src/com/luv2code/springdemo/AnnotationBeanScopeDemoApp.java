a481b36b75eff37a20c2a12a1ce95d10a1df1021
Chuangxin Lin <lcxustc@gmail.com>
2022-07-20 22:13:49 -0400

    Section 9: Spring configuration with Java Annotations - Bean Scopes Annotation.

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same objext: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		// close context
+		context.close();
+
+	}
+
+}
