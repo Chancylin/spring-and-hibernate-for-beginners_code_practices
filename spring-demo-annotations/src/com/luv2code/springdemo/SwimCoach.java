2ca03d178d02e384315488488778bdff5cd3e9ec
Chuangxin Lin <lcxustc@gmail.com>
2022-07-21 17:31:26 -0400

    Section 10: Spring configuration. Define Spring beans with Java code (no xml).

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -4,21 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
 public class SwimCoach implements Coach {
 
-	@Autowired
-	@Qualifier("fileFortuneService")
 	private FortuneService fortuneService;
 	
 
-	public SwimCoach() {
+	public SwimCoach(FortuneService theFortuneService) {
 		System.out.println(">> SwimCoach: inside default constructor");
+		fortuneService = theFortuneService;
 	}
 	
 	@Override
 	public String getDailyWorkout() {
-		return "Swin 10 km";
+		return "Swin 1000 meters as a warm up.";
 	}
 
 	@Override
