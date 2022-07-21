b7d73b64f3b0c96bac5a180b66f4ab6a71d72e1b
Chuangxin Lin <lcxustc@gmail.com>
2022-07-20 22:45:03 -0400

    Section 9: Spring configuration with Java Annotations - Bean Lifecycle Method Annotations.

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -16,6 +18,17 @@ public class TennisCoach implements Coach {
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupstuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
 	}
 	
 	/*
