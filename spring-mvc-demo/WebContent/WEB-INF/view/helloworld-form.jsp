a657d243765e414cb001d67a1c2443220038e986
Chuangxin Lin <lcxustc@gmail.com>
2022-08-02 18:15:17 -0400

    Section13: Spring MVC. Request Params and Request Mapping.

--- spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -7,7 +7,7 @@
 
 <body>
 
-    <form action="processFormVersionTwo" method="GET">
+    <form action="processFormVersionThree" method="GET">
     
         <input type="text" name="studentName"
             placeholder="What's your name?" />
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
@@ -25,6 +27,7 @@ public class HelloWorldController {
 	// add data to the model
 	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
 		// read the request parameter from the HTML form
 		String theName = request.getParameter("studentName");
 		
@@ -33,6 +36,23 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName,
+			Model model) {
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
