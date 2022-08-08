c7c51f2cc2f894913b39e9144c44ac59b12e39a4
Chuangxin Lin <lcxustc@gmail.com>
2022-08-08 13:58:38 -0400

    Section 27: Hibernate Advanced Mapping. ManyToMany.

---
+++ hb-05-many-to-many/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,30 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		// for MySQL, need &serverTimezone=UTC
+		String jdbcurl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pwd = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: "+ jdbcurl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcurl, user, pwd);
+			
+			System.out.println("Connection successful!!!");
+			
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
