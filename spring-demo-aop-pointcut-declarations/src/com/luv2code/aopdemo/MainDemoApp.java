a7c91aa300bfe7acef320e9029e382c58abde54e
Chuangxin Lin <lcxustc@gmail.com>
2022-08-09 18:50:42 -0400

    Section 38: AOP. Pointcut declarations.

---
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,35 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// read Spring config java file
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get bean from Spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// get bean from Spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		// call the membershipDAO business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		// close context
+		context.close();
+
+	}
+
+}
---
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,20 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": DOING MY DB WORK: ADD ACCOUNT");
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		
+		return false;
+	}
+}
