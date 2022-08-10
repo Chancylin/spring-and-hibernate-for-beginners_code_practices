84b5bb60645764e00e0b98020d9c71b5cf137c5b
Chuangxin Lin <lcxustc@gmail.com>
2022-08-10 13:14:39 -0400

    Section 44: AOP. @Around Advice type. Handling Exceptions.

---
+++ spring-demo-aop-zzz-around-handle-exception/src/com/luv2code/aopdemo/DemoConfig.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@EnableAspectJAutoProxy
+@PropertySource("classpath:mylogger.properties")
+@ComponentScan("com.luv2code.aopdemo")
+public class DemoConfig {
+	
+//	@Bean
+//	public MyLoggerConfig myLoggerConfig(@Value("${root.logger.level}") String rootLoggerLevel,
+//										 @Value("${printed.logger.level}") String printedLoggerLevel) {
+//
+//		MyLoggerConfig myLoggerConfig = new MyLoggerConfig(rootLoggerLevel, printedLoggerLevel);
+//
+//		return myLoggerConfig;
+//	}
+	
+}
