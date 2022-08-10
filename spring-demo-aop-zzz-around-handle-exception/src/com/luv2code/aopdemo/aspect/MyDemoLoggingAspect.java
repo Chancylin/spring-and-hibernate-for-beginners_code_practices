209074ee70cd9c2cce4e624daf50e51f7e812a8f
Chuangxin Lin <lcxustc@gmail.com>
2022-08-10 13:21:09 -0400

    Section 44: AOP. @Around Advice type. Rethrowing Exceptions.

--- spring-demo-aop-zzz-around-handle-exception/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-zzz-around-handle-exception/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -123,9 +123,8 @@ public class MyDemoLoggingAspect {
 			// log the exception
 			myLogger.warning(e.getMessage());
 
-			// give user a custom message
-			result = "Major accident! But no worries, " 
-			+ "your private AOP helicopter is on the way!";
+			// rethrow exception
+			throw e;
 		}
 		
 		// get end timestamp
