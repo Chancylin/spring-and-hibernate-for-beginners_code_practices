601ac113c15018fdc71d93ff821ec2ac6e9c3b6a
Chuangxin Lin <lcxustc@gmail.com>
2022-08-09 22:30:25 -0400

    Section 39: AOP. Ordering Aspects.

---
+++ spring-demo-aop-pointcut-order-aspects/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	// this is where we add all of our related advices for logging
+	
+	// lets start with an @Before advice
+	
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n======>>> Executing @Before advice on method");
+	}
+
+}
