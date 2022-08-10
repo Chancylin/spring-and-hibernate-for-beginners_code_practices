509f2d4f89b1b25cedc0163e55ce6f39b4002bba
Chuangxin Lin <lcxustc@gmail.com>
2022-08-09 23:46:59 -0400

    Section 41: AOP. @AfterReturning Advice type.

---
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,64 @@
+package com.luv2code.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+	
+	// add a new method: findAccounts()
+	
+	public List<Account> findAccounts() {
+		List<Account> myAccounts = new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		// add them to our account list
+		Collections.addAll(myAccounts, temp1, temp2, temp3);
+		
+		return myAccounts;
+	}
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": DOING MY DB WORK: ADD ACCOUNT");
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		
+		return false;
+	}
+
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+	
+}
