3088c6879f247dd4c4a07f11916c042301bf3f16
Chuangxin Lin <lcxustc@gmail.com>
2022-08-29 22:42:46 -0400

    Section 67: Spring REST. CRUD database real-time project - update customer.

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,13 +41,9 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
-	// add mapping for POST /customers - add new customer
-	@PostMapping("/customers")
-	public Customer addCustomer(@RequestBody Customer theCustomer) {
-		
-		// also just in case the pass an id in JSON ... set id to 0
-		// this is to force a save of new item ... instead of update
-		theCustomer.setId(0);
+	// add mapping for PUT /customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		
 		customerService.saveCustomer(theCustomer);
 		
