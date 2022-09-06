11499ead47706d3fdf90fc48abbde5c6efb6b6a7
Chuangxin Lin <lcxustc@gmail.com>
2022-09-06 17:50:51 -0400

    Section 71: Spring Boot. Spring Boot Dev Tools and Spring Boot Actuator. Add Spring Boot Dev so it will automatically monitor the code change and we don't need to restart server.

---
+++ 02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	// expose "/" that returns "Hello World"
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
+}
